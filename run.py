126f4b4dd937df16bd6168918fbd2f5561bf1331
gavr1988 <gavrewt@gmail.com>
2026-06-29 15:03:18 +0100

    converting gender to binary

--- run.py
+++ run.py
@@ -24,8 +24,16 @@ print (df.isnull().sum())
 #Cleaning Columns titles
 df.columns = df.columns.str.strip()
 df.columns = df.columns.str.replace (' ', '_')
-
+#checking columns
 print (df.columns)
+
+#Changing M to 1 and F to 0 in the Gender Column
+#This is to convert the Gender column into a binary format for analysis
+
+df ['GENDER'] = df['GENDER'].map({
+        'M': 0,
+        'F': 0
+})
 
 #exporting to a new 'cleaned' csv file
 df.to_csv ('cleaned_survey_lung_cancer.csv', index=False)
