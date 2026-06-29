55ee212c821c709ff158564bda7e6fd1b77b19bb
gavr1988 <gavrewt@gmail.com>
2026-06-29 15:17:00 +0100

    cleaned data ready for ML pipeline

--- run.py
+++ run.py
@@ -1,32 +1,42 @@
+#installing the required libraries
 import pandas as pd
 
-#loading the data
-df=pd.read_csv('survey lung cancer.csv')
 
-#printing column headers
-print(df.columns)
+def main():
+	#loading the data
+	df=pd.read_csv('survey lung cancer.csv')
 
-print (df.head)
+	#printing column headers
+	print(df.columns)
 
-#checking column data types
-print (df.dtypes)
+	print (df.head)
 
-#identifying duplicates
-duplicates = df[df.duplicated()]
-print ('Number of duplicate rows: ', duplicates.shape[0])
+	#checking column data types
+	print (df.dtypes)
 
-#Whilst there may be duplicate rows, we will not remove them as they may represent valid data points. Instead, we will keep them for analysis.
+	#identifying duplicates
+	duplicates = df[df.duplicated()]
+	print ('Number of duplicate rows: ', duplicates.shape[0])
 
-# checking for missing  values
-print("Missing Values:")
-print (df.isnull().sum())
+	#Whilst there may be duplicate rows, we will not remove them as they may represent valid data points. Instead, we will keep them for analysis.
 
-#Cleaning Columns titles
-df.columns = df.columns.str.strip()
-df.columns = df.columns.str.replace (' ', '_')
-#checking columns
-print (df.columns)
+	# checking for missing  values
+	print("Missing Values:")
+	print (df.isnull().sum())
 
-#exporting to a new 'cleaned' csv file
-df.to_csv ('cleaned_survey_lung_cancer.csv', index=False)
+	#Cleaning Columns titles
+	df.columns = df.columns.str.strip()
+	df.columns = df.columns.str.replace (' ', '_')
+	#checking columns
+	print (df.columns)
+
+	#Save the cleaned dataframe to a new CSV file so the updated data can be reused later
+	#exporting to a new 'cleaned' csv file
+	df.to_csv ('cleaned_survey_lung_cancer.csv', index=False)
+	print('Cleaned CSV saved to cleaned_survey_lung_cancer.csv')
+
+# if_name__ == '__main__': this is to ensure that the main function is only executed when the script is run directly, and not when it is imported as a module in another script.
+if __name__ == '__main__':
+	main()
+
 
