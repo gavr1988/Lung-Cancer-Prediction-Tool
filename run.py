97b40cc2ae5de5997a65450ceebcb0b4a83baebd
gavr1988 <gavrewt@gmail.com>
2026-06-29 14:55:56 +0100

    data types

--- run.py
+++ run.py
@@ -6,9 +6,19 @@ df=pd.read_csv('survey lung cancer.csv')
 #printing column headers
 print(df.columns)
 
+print (df.head)
+
+#checking column data types
+print (df.dtypes)
+
 #identifying duplicates
 duplicates = df[df.duplicated()]
 print ('Number of duplicate rows: ', duplicates.shape[0])
 
 #Whilst there may be duplicate rows, we will not remove them as they may represent valid data points. Instead, we will keep them for analysis.
 
+# checking for missing  values
+print("Missing Values:")
+print (df.isnull().sum())
+
+#
